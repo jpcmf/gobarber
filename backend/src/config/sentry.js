9c739a93db6f26a42e264a44bee65f00254c4789
João Paulo <musicjoaopaulo@gmail.com>
2019-12-11 20:36:03 -0300

    Add backend v1 app

---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://b66d3a266fe84711aa606ea19b7d96b3@sentry.io/1799327',
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import FileController from './app/controllers/FileController';
+import SessionController from './app/controllers/SessionController';
+import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
+import AvailableController from './app/controllers/AvailableController';
+import AppointmnetController from './app/controllers/AppointmentController';
+import NotificationController from './app/controllers/NotificationController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', AppointmnetController.index);
+routes.post('/appointments', AppointmnetController.store);
+routes.delete('/appointments/:id', AppointmnetController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
