88a4c37718d5e2ef47e8f419626db6b41cb82628
João Paulo <musicjoaopaulo@gmail.com>
2019-12-12 15:47:36 -0300

    Add global styles

---
+++ frontend/src/styles/global.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    color: #222;
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
