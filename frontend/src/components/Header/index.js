48640394c2e1ca2cb40eac940eae38cf8caea79a
João Paulo <musicjoaopaulo@gmail.com>
2019-12-16 13:23:17 -0300

    Create Header component

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as LogoGoBarber } from '../../assets/logo-roxa.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <LogoGoBarber className="logo-gobarber" />
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>João Paulo Fricks</strong>
+              <Link to="/profile">Meu perfil </Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="João Paulo Fricks"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ frontend/src/components/Header/styles.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #fff;
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  align-items: center;
+  display: flex;
+  height: 64px;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 900px;
+
+  nav {
+    align-items: center;
+    display: flex;
+
+    svg {
+      margin-right: 20px;
+    }
+
+    a {
+      border-left: solid 1px #eee;
+      color: #7159c1;
+      font-weight: bold;
+      padding-left: 20px;
+      text-transform: uppercase;
+    }
+  }
+
+  aside {
+    align-items: center;
+    display: flex;
+  }
+`;
+
+export const Profile = styled.div`
+  border-left: solid 1px #eee;
+  display: flex;
+  margin-left: 20px;
+  padding-left: 20px;
+
+  div {
+    margin-right: 10px;
+    text-align: right;
+
+    strong {
+      color: #333;
+      display: block;
+    }
+
+    a {
+      color: #999;
+      display: block;
+      font-size: 12px;
+      margin-top: 2px;
+    }
+  }
+
+  img {
+    border-radius: 50%;
+    height: 32px;
+  }
+`;
