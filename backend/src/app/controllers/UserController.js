801230b4a20a64a47b8e75dddedec1618a30906f
João Paulo <musicjoaopaulo@gmail.com>
2020-01-28 22:55:20 -0300

    fix: email validation [backend]

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -56,7 +56,7 @@ class UserController {
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
 
-    if (email !== user.email) {
+    if (email && email !== user.email) {
       const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
@@ -76,9 +76,9 @@ class UserController {
           model: File,
           as: 'avatar',
           attributes: ['id', 'path', 'url'],
-        }
-      ]
-    })
+        },
+      ],
+    });
 
     return res.json({
       id,
