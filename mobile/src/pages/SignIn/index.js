e323f68fc8662b273d3caf508d339654f87105a2
João Paulo <musicjoaopaulo@gmail.com>
2020-02-26 22:02:31 -0300

    feat: create the button and input components [mobile]

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 // import { Container } from './styles';
 
@@ -8,6 +10,12 @@ export default function SignIn() {
   return (
     <Background>
       <Text>Hellow</Text>
+      <Input
+        style={{ marginTop: 50 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Teste 123</Button>
     </Background>
   );
 }
