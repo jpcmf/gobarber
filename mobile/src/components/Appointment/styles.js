cb1039beac1098c95d9a467a6f2a17f924ec1b30
João Paulo <musicjoaopaulo@gmail.com>
2020-03-02 09:37:15 -0300

    feat: create the styles of the Appointment list [mobile]

---
+++ mobile/src/components/Appointment/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background-color: #fff;
+  border-radius: 4px;
+  margin-bottom: 15px;
+  padding: 20px;
+
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Left = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Avatar = styled.Image`
+  border-radius: 25px;
+  height: 50px;
+  width: 50px;
+`;
+
+export const Info = styled.View`
+  margin-left: 15px;
+`;
+
+export const Name = styled.Text`
+  color: #333;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export const Time = styled.Text`
+  color: #999;
+  font-size: 13px;
+  margin-top: 4px;
+`;
